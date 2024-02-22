c495757421a032ee21bb18d23a83751b52b0a4ac
RCLDevelopers <rasshtech@gmail.com>
2024-02-22 08:43:49 +0300

    First Commit

--- Library-Management-System---PHP-MySql-master/admin/includes/footer.php
+++ Library-Management-System---PHP-MySql-master/admin/includes/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                   &copy; 2018 Online Library Management System |<a href="https://www.linkedin.com/in/aleksandart96/" target="_blank"  > Designed by : Aleksandar Trifunovic</a> 
+                   &copy; 2024 Online Library Management System |<a href="https://zangticsdigital.com/" target="_blank"  > Designed by : Zangtics Digital</a> 
                 </div>
 
             </div>
